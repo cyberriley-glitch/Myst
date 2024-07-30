83054a8f8a7e5a4ca29a5c57f922e6aa2d5d8cdb
Philipp Gaßner <activator112233@gmail.com>
2024-07-30 21:18:00 +0200

    well?

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ from http_srv import api
 import os
 
 async def http_app():
-    api.run(host='0.0.0.0', port=os.environ["port"])
+    port = os.environ["PORT"]
+    api.run(host='0.0.0.0', port=port)
+
+async def bot_app():
+    await bot.start(os.environ["BOT_TOKEN"]),
+
 
 if __name__ == "__main__":
 
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
+#        await bot.start(os.environ["BOT_TOKEN"])
     loop.run_until_complete(asyncio.gather(
-        bot.start(os.environ["BOT_TOKEN"]),
+        bot_app(),
         http_app()
     ))
