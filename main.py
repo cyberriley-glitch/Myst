f873f1a632ca90d3580a1e3cfbfb7505471dc268
Philipp Gaßner <activator112233@gmail.com>
2024-09-24 21:41:32 +0200

    does it work now?

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ from my_copybot import bot
 import os
 from dotenv import load_dotenv
 
-async def bot_app():
+async def main():
     print("Starting bot")
-    await bot.start(os.environ["BOT_TOKEN"]),
+    async with bot:
+        await bot.start(os.environ["BOT_TOKEN"])
 
 
 if __name__ == "__main__":
     load_dotenv()
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(asyncio.gather(
-        bot_app()
-    ))
+    main()
